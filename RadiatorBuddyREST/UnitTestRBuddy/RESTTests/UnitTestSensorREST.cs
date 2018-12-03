add3553780d48eac657d9db1a9977331faad5bcd
Weyrun <Mikkelkroyer@hotmail.com>
2018-12-03 09:33:46 +0100

    Worked on some tests

--- RadiatorBuddyREST/UnitTestRBuddy/RESTTests/UnitTestSensorREST.cs
+++ RadiatorBuddyREST/UnitTestRBuddy/RESTTests/UnitTestSensorREST.cs
@@ -55,21 +55,42 @@ namespace UnitTestRBuddy.RESTTests
         }
 
         [TestMethod]
-        public void Post_needstopass()
+        public void GetOne_needstofail()
         {
             var client = new HttpClient(); // no HttpServer
 
             var request = new HttpRequestMessage
             {
+                RequestUri = new Uri("http://localhost:63998/api/SensorsData/999"),
+                Method = HttpMethod.Get
+            };
+
+            //request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            using (var response = client.SendAsync(request).Result)
+            {
+                Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public void Post_needstopass()
+        {
+            var client = new HttpClient(); // no HttpServer
+
+            var send = new HttpRequestMessage
+            {
                 RequestUri = new Uri("http://localhost:63998/api/SensorsData/"),
                 Method = HttpMethod.Post
             };
 
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            PiData newpi = new PiData("55", 20, DateTime.Now, "here", true);
+            string jstring = JsonConvert.SerializeObject(newpi);
+            send.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(jstring));
 
-            using (var response = client.SendAsync(request).Result)
+            using (var response = client.SendAsync(send).Result)
             {
-                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+                Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
             }
         }
     }
